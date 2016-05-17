c35aec744ac1e887e9f70f63ca6d751cf7388b40
krinanagar <krina_nagar95@hotmail.co.nz>
2016-05-17 13:39:51 +1200

    fixed register mistake

--- routes/index.js
+++ routes/index.js
@@ -58,7 +58,7 @@ router.get('/register', function(req, res) {
   res.render('register', { title: 'Register' });
 });
 
-router.post('/login', function (req,res,next) {
+router.post('/register', function (req,res,next) {
     console.log("Trying to log in");
     	  var NAME = req.body.name;
         var USERNAME = req.body.user;
