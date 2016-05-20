9f2ef14ac85194703b71992c2fb3095c5b84132d
thepangman <azzapang40@yahoo.com>
2016-05-20 18:31:18 +1200

    Just bought some bleach

--- routes/browse.js
+++ routes/browse.js
@@ -5,9 +5,35 @@ var database =  "postgres://mxoilrnicwhdji:OhBRE_r8LgodxHHZ_ROjGFukd4@ec2-54-163
 var userName = null;
 
 /*Navigate to browse page */
-router.get("/",function(req,res){
-  res.render('browse', { title: 'Browse', username: userName });
-});
+// router.get("/",function(req,res){
+//   res.render('browse', { title: 'Browse', username: userName });
+// });
 
+router.get("/",function(req,res) {
+  var items = [];
+  pg.connect(database, function (err, client, done) {
+    // Query items
+    var query = client.query("SELECT * FROM Stock", function (err, result) {
+      console.log("fuckboi");
+      // For each item
+      for (i = 0; i < result.rows.length; i++) {
+        // Add item
+        var item = {
+          uid: result.rows[i].uid,
+          label: result.rows[i].label,
+          summary: result.rows[i].summary,
+          price: result.rows[i].price,
+        };
+        items.push(item);
+        console.log(items);
+      }
+    });
+
+    query.on('end', function () {
+      res.render('browse', {title: "Browse", items: items});
+      done();
+    })
+  })
+});
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var pg = require('pg').native;
 var database =  "postgres://mxoilrnicwhdji:OhBRE_r8LgodxHHZ_ROjGFukd4@ec2-54-163-248-14.compute-1.amazonaws.com:5432/d8dqj27651vg99";
 var userName = null;
+var buy = null;
 
 /*GET profile page*/
 router.get('/', function(req,res){
@@ -12,13 +13,14 @@ router.get('/', function(req,res){
             return console.error('could not connect to postgres', err);
         }
         console.log('Connected to database');
-        var query = "SELECT * FROM stock ORDER BY label";
+        var query = "SELECT * FROM stock";
         client.query(query, function(error, result){
             if(error) {
                 console.error('Query failed');
                 console.error(error);
                 return;
             }
+            buy = result;
         })
     })
     res.render('profile', { title: 'Profile', username: userName });
