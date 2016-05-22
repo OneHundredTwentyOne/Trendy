38f9570c5e541edd46c6409c94acb5efcd9e5162
thepangman <azzapang40@yahoo.com>
2016-05-22 22:03:55 +1200

    Starting on cart

--- routes/item.js
+++ routes/item.js
@@ -5,7 +5,11 @@ var database =  "postgres://mxoilrnicwhdji:OhBRE_r8LgodxHHZ_ROjGFukd4@ec2-54-163
 var username = null;
 var LocalStorage = require('node-localstorage').LocalStorage,
     localStorage = new LocalStorage('./scratch');
-
+var sellername = "";
+var image = "";
+var label = "";
+var price = 0;
+var size = "";
 
 /*GET item page*/
 router.get("/",function(req,res) {
@@ -21,12 +25,44 @@ router.get("/",function(req,res) {
           label: result.rows[0].label,
           description: result.rows[0].description,
           price: result.rows[0].price,
+          size: result.rows[0].size,
+          sellername: result.rows[0].sellername
         };
+      image=result.rows[0].image;
+      label= result.rows[0].label;
+      price= result.rows[0].price;
+      size= result.rows[0].size;
+      sellername= result.rows[0].sellername;
     res.render('item', {title: 'Item', items: item});
   })
   });
 });
 
+router.post('/', function (req,res,next) {
+  console.log("Adding to cart");
+  var client = new pg.Client(database);
+  pg.connect(database,function(err,client,done){
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    console.log('Connected to database');
+    var query = ("INSERT INTO Cart (sellername,image,label,size,price) VALUES ($1, $2, $3, $4, $5)");
+    client.query(query,[sellername,image,label,size,price], function(error, result){
+      console.log(sellername);
+      console.log(error);
+      if(error) {
+        console.error('Query failed');
+        console.error(error);
+        return;
+      }
+      else{
+        localStorage.setItem("username",username);
+        res.render('item', { title:  'Item', items:item });
+        return;
+      }
+    })
+  })
+});
 
 
 module.exports = router;
