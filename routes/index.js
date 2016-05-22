ecdc9e0cda41efb69f40daab8c3d98d3322121a3
krinanagar <krina_nagar95@hotmail.co.nz>
2016-05-23 11:05:18 +1200

    fixed logout

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg').native;
 var database =  "postgres://mxoilrnicwhdji:OhBRE_r8LgodxHHZ_ROjGFukd4@ec2-54-163-248-14.compute-1.amazonaws.com:5432/d8dqj27651vg99";
-var username = null;
+var username =  null;
 var LocalStorage = require('node-localstorage').LocalStorage,
     localStorage = new LocalStorage('./scratch');
 
