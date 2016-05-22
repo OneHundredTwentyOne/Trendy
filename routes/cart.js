9aa695f0dcb4b9cdceb298caa1be507457d4404f
thepangman <azzapang40@yahoo.com>
2016-05-22 22:38:14 +1200

    Aye the cart works lmao

--- routes/cart.js
+++ routes/cart.js
@@ -5,10 +5,30 @@ var database =  "postgres://mxoilrnicwhdji:OhBRE_r8LgodxHHZ_ROjGFukd4@ec2-54-163
 var username = null;
 var LocalStorage = require('node-localstorage').LocalStorage,
     localStorage = new LocalStorage('./scratch');
+var totalPrice = 0;
 
 /*GET profile page*/
 router.get('/', function(req,res){
-    res.render('cart', { title: 'Cart', username: username });
+    var items = [];
+    username = localStorage.getItem("username");
+    pg.connect(database, function (err, client, done) {
+        var query = client.query("SELECT * FROM CART WHERE USERNAME = '%username%';".replace('%username%',username), function (err, result) {
+            for (var i = 0; i < result.rows.length; i++) {
+                var item = {
+                    uid: result.rows[i].uid,
+                    image: result.rows[i].image,
+                    label: result.rows[i].label,
+                    summary: result.rows[i].summary,
+                    price: result.rows[i].price,
+                    sellername: result.rows[i].sellername,
+                    description: result.rows[i].description
+                };
+                totalPrice = +totalPrice + +result.rows[i].price;
+                items.push(item);
+            }
+            res.render('cart', { title: 'Cart', username: username, items: items, totalPrice: totalPrice });
+        })
+    });
 });
 
 
--- routes/item.js
+++ routes/item.js
@@ -5,6 +5,7 @@ var database =  "postgres://mxoilrnicwhdji:OhBRE_r8LgodxHHZ_ROjGFukd4@ec2-54-163
 var username = null;
 var LocalStorage = require('node-localstorage').LocalStorage,
     localStorage = new LocalStorage('./scratch');
+var item = null;
 var sellername = "";
 var image = "";
 var label = "";
@@ -19,7 +20,7 @@ router.get("/",function(req,res) {
   pg.connect(database, function (err, client, done) {
     // Query items
     var query = client.query("SELECT * FROM Stock WHERE uid = " + id, function (err, result) {
-        var item = {
+        item = {
           uid: result.rows[0].uid,
           image: result.rows[0].image,
           label: result.rows[0].label,
@@ -46,8 +47,8 @@ router.post('/', function (req,res,next) {
       return console.error('could not connect to postgres', err);
     }
     console.log('Connected to database');
-    var query = ("INSERT INTO Cart (sellername,image,label,size,price) VALUES ($1, $2, $3, $4, $5)");
-    client.query(query,[sellername,image,label,size,price], function(error, result){
+    var query = ("INSERT INTO Cart (sellername,image,label,size,price, username) VALUES ($1, $2, $3, $4, $5, $6)");
+    client.query(query,[sellername,image,label,size,price,username], function(error, result){
       console.log(sellername);
       console.log(error);
       if(error) {
